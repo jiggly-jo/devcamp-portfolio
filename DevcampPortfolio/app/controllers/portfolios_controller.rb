3d4435e61337c37f24194a4acd84495d36df7e1c
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 16:55:06 +0000

    Implemented Destroy action for portoflio feature

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -35,4 +35,14 @@ end
     @portfolio_item = Portfolio.find(params[:id])
     
 end
+  def destroy
+    #Perfom the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+    #destoys the record
+        @portfolio_item.destroy
+        #redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Blog was successfully destroyed.' }
+    end
+end
 end
