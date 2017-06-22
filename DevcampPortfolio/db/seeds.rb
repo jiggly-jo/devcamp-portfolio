4356c9d38e8bfe4f70f98c96acc676bf6ea728a1
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:58:38 +0000

    Integrated technologies model

--- DevcampPortfolio/db/seeds.rb
+++ DevcampPortfolio/db/seeds.rb
@@ -43,3 +43,8 @@ end
     )
 end
   puts "9 items created"
+  
+  3.times do |technology|
+     Portfolio.last.technologies.create!(name: "Technology#{technology}")
+  end
+  puts "3 technologies created"
